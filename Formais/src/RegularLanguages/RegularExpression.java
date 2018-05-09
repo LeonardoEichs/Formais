86101093996a0ff768af107f2b932cd3aaa271a7
Gustavo Gava <gustavogarciagava@gmail.com>
2018-05-09 14:56:25 -0300

    Regular Expressions validation method

--- Formais/src/RegularLanguages/RegularExpression.java
+++ Formais/src/RegularLanguages/RegularExpression.java
@@ -1,39 +1,68 @@
 package RegularLanguages;
 
+import RegularLanguages.RegularLanguage.InputType;
+
 public class RegularExpression extends RegularLanguage{
 
-	public RegularExpression(String input, InputType type) {
-		super(input, type);
-		// TODO Auto-generated constructor stub
+	private String re;
+	
+	public RegularExpression(String input) {
+		super(input, InputType.RE);
+		this.re = input;
 	}
 
 	public static RegularLanguage isValidRE(String inp) {
-		// TODO Auto-generated method stub
-		return null;
+		RegularExpression rl = new RegularExpression(inp);
+		if(validateInput(inp)) {
+			return rl;
+		} else {
+			return null;
+		}
+		
 	}
 
 	@Override
 	public String getDefinition() {
-		// TODO Auto-generated method stub
-		return null;
+		return re;
 	}
 
 	@Override
 	public RegularGrammar getRG() {
-		// TODO Auto-generated method stub
+		// toRG;
 		return null;
 	}
 
 	@Override
 	public RegularExpression getRE() {
-		// TODO Auto-generated method stub
-		return null;
+		return this;
 	}
 
 	@Override
 	public FiniteAutomata getFA() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public static boolean validateInput(String inp) {
+		inp = inp.replaceAll("\\s+", "");
+		
+		if(!inp.matches("^[a-z0-9\\(\\)\\?\\+\\*\\|\\.\\&]*")) {
+			return false;
+		}
+		
+		int pcount = 0;
+		for (int i = 0; i < inp.length(); i++) {
+			char c = inp.charAt(i);
+			if (c == '(') {
+				pcount++;
+			} else if (c == ')') {
+				pcount--;
+				if (pcount < 0) {
+					return false;
+				}
+			}
+		}
+		return pcount == 0;
 	}
 
 }
